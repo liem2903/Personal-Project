0c4b3ee322dcba9486880389c9ea657a68edba8c
Liem Phan <z5419546@student.unsw.edu.au>
2024-04-26 17:51:17 +1000

    Create JFrame and JPanel for game.

---
+++ my2d_game/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Adventuring in 2D");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); // Resizes window to fit the size of game panel.
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
